f9c9f01288ce5641a215adb1760746232ceb08d3
yudete <yu@yude.moe>
2020-10-11 15:37:32 +0900

    Update gcd.c

--- DA1/gcd.c
+++ DA1/gcd.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int gcd(int cal_1; int cal_2){
-    
-}
+
 int main(){
     int num_1, num_2; // 比較する 2 つの数
-    int cal_1, cal_2; // 処理時に実際に処理する値
+    int tmp, cal; // 処理時に使用する値
     int skip = 0; // 処理を飛ばすフラグ
 
     // 比較対象の数値を格納する
@@ -14,20 +12,19 @@ int main(){
     scanf("%d", &num_2);
 
     // 入力された数値の大小を比較する
-    if (num_1 > num_2){
-        cal_1 = num_1;
-        cal_2 = num_2;
-    }else if (num_1 < num_2){
-        cal_1 = num_2;
-        cal_2 = num_1;
-    }
-    else{
-        skip = 1;
-        cal_2 = num_1;
+    if (num_1 < num_2){
+        tmp = num_1;
+        num_1 = num_2;
+        num_2 = tmp;
     }
 
-    for(;;){
+    cal = num_1 % num_2;
+
+    while (cal != 0){
+       num_1 = num_2;
+       num_2 = cal;
+       cal = num_1 % num_2;
     }
 
-    printf("GCD is %d", cal_1);
+    printf("GCD is %d", num_2);
 }
