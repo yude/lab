58acb305034776c90daf2272fcbbd28089225fca
yudejp <i@yude.jp>
2021-04-25 22:12:54 +0900

    Add files

---
+++ rust-lang/hello-world/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    
+    let a_number = 10;
+    let a_boolean = true;
+    
+    println!("the number is {}.", a_number);
+    println!("the boolean is {}.", a_boolean);
+}
